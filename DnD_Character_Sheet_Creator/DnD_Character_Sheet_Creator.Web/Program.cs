2fc9fb173b50185d276500b5be55568d1b912be1
David Tenšek <dtensek@tvz.hr>
2026-05-07 17:58:08 +0200

    Populated the databases

--- DnD_Character_Sheet_Creator/DnD_Character_Sheet_Creator.Web/Program.cs
+++ DnD_Character_Sheet_Creator/DnD_Character_Sheet_Creator.Web/Program.cs
@@ -1,6 +1,7 @@
 using DnD_Character_Sheet_Creator.Data;
 using DnD_Character_Sheet_Creator.Repositories;
 using Microsoft.EntityFrameworkCore;
+using System.Linq;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -20,6 +21,43 @@ using (var scope = app.Services.CreateScope())
 {
     var context = scope.ServiceProvider.GetRequiredService<DnDDbContext>();
     context.Database.Migrate();
+    // Seed database from mock repositories if empty
+    if (!context.Players.Any())
+    {
+        var players = MockRepositorySeed.CreatePlayers();
+        var charactersByPlayer = MockRepositorySeed.CreateCharactersByPlayerId();
+
+        foreach (var player in players)
+        {
+            // preserve original id for lookup, then clear identity so SQL Server assigns one
+            var originalPlayerId = player.PlayerId;
+            player.PlayerId = 0;
+
+            if (charactersByPlayer.TryGetValue(originalPlayerId, out var chars))
+            {
+                foreach (var ch in chars)
+                {
+                    ch.CharacterId = 0;
+                    if (ch.Level != null)
+                    {
+                        ch.Level.LevelId = 0;
+                    }
+
+                    foreach (var eq in ch.EquipmentList)
+                    {
+                        eq.EquipmentId = 0;
+                    }
+
+                    // ensure FK will be set by relationship
+                    ch.Player = player;
+                    player.CharacterList.Add(ch);
+                }
+            }
+        }
+
+        context.Players.AddRange(players);
+        context.SaveChanges();
+    }
 }
 
 if (!app.Environment.IsDevelopment())
